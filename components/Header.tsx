d146077863d58fc02711664a58114d63dd78ce61
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-08-10 17:22:06 +0530

    basic header layout made

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function Header({ children }: HeaderProps) {
+    return (
+        <div className='header'>
+            <Link href={'/'} className='md:flex-1'>
+                <Image
+                    src={'/assets/icons/logo.svg'}
+                    width={120}
+                    height={32}
+                    className='cursor-pointer hidden md:block'
+                    alt='logo with name'
+                />
+                <Image
+                    src={'/assets/icons/logo-icon.svg'}
+                    width={32}
+                    height={32}
+                    className='cursor-pointer mr-2 md:hidden'
+                    alt='logo without name'
+                />
+            </Link>
+            {children}
+        </div>
+    )
+}
+
+export default Header
